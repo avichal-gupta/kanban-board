741be0ed18e4e1db34687eccb83d5cf671806535
avichal-gupta <basugupta1000@gmail.com>
2023-12-10 01:37:17 +0530

    deploy

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const UserAvatar = ({ userId }) => {
-  // You'll need to fetch the user's avatar URL or use a placeholder
+  
   const avatarUrl = 'https://eu.ui-avatars.com/api/?name=John+Doe&size=250';
   return (
     <img src={avatarUrl} alt="User Avatar" className="user-avatar" />
